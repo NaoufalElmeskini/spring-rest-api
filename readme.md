bca4cbe2d3e519328d86c7272ea4bc34261a76b2
tintin <naoufal.elmeskini@gmail.com>
2024-11-20 09:05:26 +0100

    ajouter controller /rest-api/product/1

--- readme.md
+++ readme.md
@@ -1,8 +1,16 @@
 
+local baseUrl : http://localhost:8080
 
-tester en local : http://localhost:8080/rest-api/status 
+usecase 0 : statut de l'application
+    url : {baseUrl}/rest-api/status 
+
+usecase 1 : recuperer produit 
+    prerequis : demarrer price api
+    url : {baseUrl}/rest-api/product/1
+
 
 ---
 backlog désiré : 
 - ajouter wiremock
+- ajouter swagger
 - organiser en architecture hexagonale
--- src/main/java/io/lacrobate/spring_rest_api/SpringRestApiApplication.java
+++ src/main/java/io/lacrobate/spring_rest_api/SpringRestApiApplication.java
@@ -2,6 +2,8 @@ package io.lacrobate.spring_rest_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringRestApiApplication {
@@ -10,4 +12,8 @@ public class SpringRestApiApplication {
 		SpringApplication.run(SpringRestApiApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
