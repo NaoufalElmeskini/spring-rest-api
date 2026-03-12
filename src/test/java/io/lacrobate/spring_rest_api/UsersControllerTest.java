1568cf0856e68008c96ad550d6e4a2ff624b0e88
NaoufalELMESKINI <naoufal.elmeskini@gmail.com>
2026-03-12 15:02:25 +0100

    adding MVC integration test

--- src/test/java/io/lacrobate/spring_rest_api/UsersControllerTest.java
+++ src/test/java/io/lacrobate/spring_rest_api/UsersControllerTest.java
@@ -1,5 +1,7 @@
 package io.lacrobate.spring_rest_api;
 
+import io.lacrobate.spring_rest_api.client.JsonPlaceholderClient;
+import io.lacrobate.spring_rest_api.controller.UsersController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
